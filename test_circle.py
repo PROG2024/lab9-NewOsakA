8dceb52e6da5d3f6bc89468e28cbc76cafcf2125
NewOsakA <115053961+NewOsakA@users.noreply.github.com>
2024-03-12 16:03:54 +0700

    test

--- test_circle.py
+++ test_circle.py
@@ -7,6 +7,33 @@ Write unit tests as described in README.md.
 3. Unit test that circle constructor raises exception of radius is negative.
 
 """
+import unittest
 from circle import Circle
 
-# TODO write 3 tests as described above
+
+class CircleTest(unittest.TestCase):
+    def setUp(self):
+        self.c1 = Circle(7)
+        self.c2 = Circle(24)
+        self.c3 = Circle(25)
+
+    def test_add_area_with_typical_values(self):
+        """test for add_area method by creating new circle from existing circle"""
+        new_circle = self.c1.add_area(self.c2)
+        self.assertEqual(new_circle.get_radius(), self.c3.get_radius())
+        self.assertEqual(new_circle.get_area(), self.c3.get_area())
+
+    def test_add_area_edge_case(self):
+        """test for add area method where some circle has radius 0"""
+        # create new circle that radius is 0
+        cZero = Circle(0)
+        new_circle = self.c1.add_area(cZero)
+        self.assertEqual(new_circle.get_radius(), self.c1.get_radius())
+        self.assertEqual(new_circle.get_area(), self.c1.get_area())
+
+    def test_create_circle_with_negative_radius(self):
+        """test for circle constructor with negative radius"""
+        with self.assertRaises(ValueError):
+            c_temp1 = Circle(-1)
+        with self.assertRaises(ValueError):
+            c_temp2 = Circle(-100)
